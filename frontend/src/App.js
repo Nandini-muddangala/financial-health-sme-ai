34ce383fd19a778adce7d6270dd84c4d45a0ba95
Nandini-muddangala <nandhiniprabhu2005@gmail.com>
2026-02-06 17:59:09 +0530

    Use backend URL from env

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:8000/upload-financials",
+        "`${process.env.REACT_APP_API_URL}/upload-financials`",
         {
           method: "POST",
           body: formData,
